17ad2328169b00c624a646ab70132d1f4ca77409
Harry Ng <nguyenharry1907@gmail.com>
2020-11-24 09:07:43 -0700

    update PM FinInNote style full edit 25nov20

---
+++ PM-Webpage/script.js
@@ -0,0 +1,31 @@
+function myFunctionX(z) {
+            z.classList.toggle("change");
+          var x = document.getElementById("myTopnav");
+          if (x.className === "main-menu") {
+            x.className += " responsive";
+          } else {
+            x.className = "main-menu";
+          }
+        }
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var myTopnav = document.getElementById("myTopnav");
+
+// Get the offset position of the navbar
+var sticky = myTopnav.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+
+function myFunction() {
+
+
+  if (window.pageYOffset >= sticky-13) {
+
+    myTopnav.classList.add("sticky")
+  } else {
+    myTopnav.classList.remove("sticky");
+  }
+}
